220eb24562f7eab5ada57886308a1c254cd1021c
corradoo <konrado317@gmail.com>
2020-12-17 15:33:43 +0100

    Prosty mechanizm wykonywania ruchów

--- ClientApp/src/main/java/pl/checkers/App.java
+++ ClientApp/src/main/java/pl/checkers/App.java
@@ -16,6 +16,9 @@ public class App extends Application {
     private ArrayList<Circle> circles = new ArrayList<>();
     private ArrayList<Circle> chequers = new ArrayList<>();
     private Circle current = null;
+    private Circle currChequer = null;
+
+    private int yGap = 45;
 
     @Override
     public void start(Stage stage) {
@@ -38,7 +41,7 @@ public class App extends Application {
 
         drawBase(4+9,8,4,false,Color.ORANGE);
         drawBase(4,8,4,false,Color.CORNFLOWERBLUE);
-        drawBase(8,17,4,false,Color.RED);
+        drawBase(8,17,4,false,Color.INDIANRED);
 
 
 
@@ -54,19 +57,23 @@ public class App extends Application {
     public void drawBase( int beginX,int beginY,int h, boolean normal, Color color) {
         int endX = beginX;
         int endY;
-        Circle c;
+
         if(normal) {
             endY = beginY + h;
             for(int i = beginY; i < endY;i++) {
                 for(int j = beginX; j<=endX;j++) {
+                    Circle c;
                     if (i % 2 == 0) {
-                        c = new Circle(j * 55, i * 50, 25);
+                        c = new Circle(j * 50, i * yGap, 24);
                     } else {
-                        c = new Circle(j * 55 + 25, i * 50, 25);
+                        c = new Circle(j * 50 + 25, i * yGap, 24);
                     }
                     c.setFill(color);
                     c.setOnMouseClicked((event -> {
-                        System.out.println("chequer");
+                        if(currChequer == null) {
+                            setCurrentChequer(c);
+                            showMoves(currChequer);
+                        }
                     }));
                     chequers.add(c);
                 }
@@ -77,14 +84,18 @@ public class App extends Application {
             endY = beginY - h;
             for (int i = beginY; i > endY; i--) {
                 for (int j = beginX; j <= endX; j++) {
+                    Circle c;
                     if (i % 2 == 0) {
-                        c = new Circle(j * 55, i * 50, 25);
+                        c = new Circle(j * 50, i * yGap, 24);
                     } else {
-                        c = new Circle(j * 55 + 25, i * 50, 25);
+                        c = new Circle(j * 50 + 25, i * yGap, 24);
                     }
                     c.setFill(color);
                     c.setOnMouseClicked((event -> {
-                        System.out.println("chequer");
+                        if(currChequer == null) {
+                            setCurrentChequer(c);
+                            showMoves(currChequer);
+                        }
                     }));
                     chequers.add(c);
                 }
@@ -93,6 +104,18 @@ public class App extends Application {
 
             }
         }
+    }
+
+    private void setCurrentChequer(Circle c) {
+        for (Circle ch: chequers) {
+            if(ch.getCenterX() == c.getCenterX() && ch.getCenterY() == c.getCenterY())
+                currChequer = ch;
+
+        }
+    }
+
+    private void showMoves(Circle chequer) {
+        paintNeighbours(chequer);
     }
 
     public void drawBoard() {
@@ -104,13 +127,16 @@ public class App extends Application {
             for (int j = beginX; j < endX; j++) {
                 Circle c;
                 if (i % 2 == 0) {
-                    c = new Circle(j * 55, i * 50, 25);
+                    c = new Circle(j * 50, i * yGap, 24);
                 } else {
-                    c = new Circle(j * 55 + 25, i * 50, 25);
+                    c = new Circle(j * 50 + 25, i * yGap, 24);
                 }
                 c.setOnMouseClicked((event -> {
-                            for (Circle cir: circles)cir.setStroke(Color.BLACK);
-                            if(current == null) {
+                            if(currChequer != null) {
+                                moveChequer(c);
+                                currChequer = null;
+                            }
+                           /* if(current == null) {
                                 current = c;
                                 c.setFill(Color.GREEN);
                                 paintNeighbours(c);
@@ -121,7 +147,7 @@ public class App extends Application {
                             } else {
                                 current = null;
                                 for (Circle cir: circles)cir.setFill(Color.BLACK);
-                            }
+                            }*/
                         }));
                 circles.add(c);
             }
@@ -135,6 +161,24 @@ public class App extends Application {
         }
     }
 
+    private void moveChequer( Circle c) {
+        for (Circle ch:chequers) {
+            if(ch.getCenterX() == currChequer.getCenterX() && ch.getCenterY() == currChequer.getCenterY() &&
+                    calculateDist(currChequer,c) < 60) {
+                ch.setCenterX(c.getCenterX());
+                ch.setCenterY(c.getCenterY());
+                currChequer = null;
+                for (Circle cir: circles)cir.setStroke(Color.BLACK);
+            }
+            if (ch.getCenterX() == currChequer.getCenterX() && ch.getCenterY() == currChequer.getCenterY() && c.getStroke() == Color.RED) {
+                ch.setCenterX(c.getCenterX());
+                ch.setCenterY(c.getCenterY());
+                currChequer = null;
+                for (Circle cir: circles)cir.setStroke(Color.BLACK);
+            }
+        }
+    }
+
     public void paintNeighbours(Circle main) {
         double dist;
         double xDis;
@@ -144,6 +188,29 @@ public class App extends Application {
             yDis = c.getCenterY()-main.getCenterY();
             dist = Math.sqrt(xDis*xDis + yDis*yDis);
             if(dist < 60 && dist !=0) c.setStroke(Color.RED);
+        }
+        for (Circle ch: chequers) {
+            xDis = ch.getCenterX()-main.getCenterX();
+            yDis = ch.getCenterY()-main.getCenterY();
+            dist = Math.sqrt(xDis*xDis + yDis*yDis);
+            if(dist < 60 && dist !=0) {
+                System.out.println("Dist: "+dist);
+                showJumps(main,ch);
+            }
+        }
+    }
+
+    private void showJumps(Circle main, Circle ch) {
+
+        double xDis = ch.getCenterX() - main.getCenterX() ;
+        double yDis = ch.getCenterY() - main.getCenterY() ;
+
+        for(Circle c: circles) {
+
+            if(c.getCenterX() - ch.getCenterX() == xDis && c.getCenterY() - ch.getCenterY() == yDis) {
+                c.setStroke(Color.RED);
+                System.out.println(xDis);
+            }
         }
     }
 
