468fb5918f91b561f257a12e4b8a03877adf183d
corradoo <konrado317@gmail.com>
2020-12-17 02:41:14 +0100

    Dodanie funkcji obliczania sąsiednich pól

--- ClientApp/src/main/java/pl/checkers/App.java
+++ ClientApp/src/main/java/pl/checkers/App.java
@@ -13,11 +13,12 @@ import java.util.ArrayList;
 
 
 public class App extends Application {
+    private ArrayList<Circle> circles = new ArrayList<>();
+    private Circle current = null;
 
     @Override
     public void start(Stage stage) {
 
-        ArrayList<Circle> circles = new ArrayList<>();
 
         stage.setTitle("Drawing Operations Test");
         Group root = new Group();
@@ -26,33 +27,10 @@ public class App extends Application {
         AnchorPane overlay = new AnchorPane();
 
         /*TODO ustawić autodopasowanie do okna*/
-        circles.add(new Circle(1000,1000,1));
+        circles.add(new Circle(1000, 1000, 1));
         overlay.setStyle("-fx-background-color: #202020");
 
-
-        /*Tworzenie planszy bez baz*/
-        int beginX = 17/2-2, endX= beginX +5;
-        int beginY = 4, endY = 13, middleY = 8;
-        for(int i = beginY; i < endY; i++) {
-            for(int j = beginX; j < endX; j++) {
-                Circle c;
-                if (i % 2 == 0 ) {
-                    c = new Circle(j * 55, i * 50, 25);
-                }
-                else {
-                    c = new Circle(j * 55+25, i * 50, 25);
-                }
-                c.setOnMouseClicked((event -> c.setFill(new Color(Math.random(),Math.random(),Math.random(),Math.random()))));
-                circles.add(c);
-            }
-            if(i >= middleY) {
-                if (i % 2 == 1) beginX++;
-                else endX--;
-            } else {
-                if (i % 2 == 0) beginX--;
-                else endX++;
-            }
-        }
+        drawBoard();
 
         overlay.getChildren().addAll(circles);
 
@@ -60,6 +38,64 @@ public class App extends Application {
 
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void drawBoard() {
+
+        /*Tworzenie planszy bez baz*/
+        int beginX = 17 / 2 - 2, endX = beginX + 5;
+        int beginY = 4, endY = 13, middleY = 8;
+        for (int i = beginY; i < endY; i++) {
+            for (int j = beginX; j < endX; j++) {
+                Circle c;
+                if (i % 2 == 0) {
+                    c = new Circle(j * 55, i * 50, 25);
+                } else {
+                    c = new Circle(j * 55 + 25, i * 50, 25);
+                }
+                c.setOnMouseClicked((event -> {
+                            for (Circle cir: circles)cir.setStroke(Color.BLACK);
+                            if(current == null) {
+                                current = c;
+                                c.setFill(Color.GREEN);
+                                paintNeighbours(c);
+                            } else if (calculateDist(c,current) < 60) {
+                                current = c;
+                                c.setFill(Color.CORNFLOWERBLUE);
+                                paintNeighbours(c);
+                            } else {
+                                current = null;
+                                for (Circle cir: circles)cir.setFill(Color.BLACK);
+                            }
+                        }));
+                circles.add(c);
+            }
+            if (i >= middleY) {
+                if (i % 2 == 1) beginX++;
+                else endX--;
+            } else {
+                if (i % 2 == 0) beginX--;
+                else endX++;
+            }
+        }
+    }
+
+    public void paintNeighbours(Circle main) {
+        double dist;
+        double xDis;
+        double yDis;
+        for (Circle c: circles) {
+            xDis = c.getCenterX()-main.getCenterX();
+            yDis = c.getCenterY()-main.getCenterY();
+            dist = Math.sqrt(xDis*xDis + yDis*yDis);
+            if(dist < 60 && dist !=0) c.setStroke(Color.RED);
+        }
+    }
+
+    public double calculateDist(Circle c1, Circle c2) {
+        double xDis = c1.getCenterX()-c2.getCenterX();
+        double yDis = c1.getCenterY()-c2.getCenterY();
+        return Math.sqrt(xDis*xDis + yDis*yDis);
     }
 
     public static void main(String args[]){
