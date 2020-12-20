e14ed69c3eb7487e2e4771bfcd3f38825166aa79
corradoo <konrado317@gmail.com>
2020-12-20 17:21:20 +0100

    Dodanie JUnit

---
+++ ClientApp/src/test/java/pl/checkers/tests/CalculateDistTest.java
@@ -0,0 +1,18 @@
+package pl.checkers.tests;
+
+import javafx.scene.shape.Circle;
+import org.junit.Test;
+import pl.checkers.Board;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculateDistTest {
+
+    @Test
+    public void testCalculateDist() {
+        Board testBoard = new Board(1);
+        Circle c1 = new Circle(0,0,1);
+        Circle c2 = new Circle(1,1,1);
+        assertEquals(testBoard.calculateDist(c1,c2),Math.sqrt(2),0);
+    }
+}
