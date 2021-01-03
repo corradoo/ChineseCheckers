bf650cc6cfe7470058d1eae4fe65e5ac882e246e
sztrok <72755032+sztrok@users.noreply.github.com>
2021-01-04 00:51:33 +0100

    Dodanie warunku zwycięstwa

--- ClientApp/src/main/java/pl/server/ServerBoard.java
+++ ClientApp/src/main/java/pl/server/ServerBoard.java
@@ -46,6 +46,56 @@ public class ServerBoard {
        fields.get(start).setPlayer(0);
     }
 
+    public int getWinner() {
+        int player=0;
+        int c1 = 0,c2=0,c3=0,c4=0,c5=0,c6=0;
+        for(Field field: fields){
+            if(field.getBase()!=0){
+                if(field.getBase()==1){
+                    if(field.getPlayer()==1){
+                        c1++;
+                        if(c1==10) player=1;
+                    }
+                }
+                else if(field.getBase()==2){
+                    if(field.getPlayer()==2){
+                        c2++;
+                        if(c2==10) player=2;
+                    }
+                }
+                else if(field.getBase()==3){
+                    if(field.getPlayer()==3){
+                        c3++;
+                        if(c3==10) player=3;
+                    }
+                }
+                else if(field.getBase()==4){
+                    if(field.getPlayer()==4){
+                        c4++;
+                        if(c4==10) player=4;
+                    }
+                }
+                else if(field.getBase()==5){
+                    if(field.getPlayer()==5){
+                        c5++;
+                        if(c5==10) player=5;
+                    }
+                }
+                else if(field.getBase()==6){
+                    if(field.getPlayer()==6){
+                        c6++;
+                        if(c6==10) player=6;
+                    }
+                }
+
+
+            }
+        }
+
+        return player;
+    }
+
+
     public double calculateDist(int f1, int f2) {
         double xDis = fields.get(f1).getX() - fields.get(f2).getX();
         double yDis = fields.get(f1).getY() - fields.get(f2).getY();
